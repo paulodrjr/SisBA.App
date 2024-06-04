ac6f5b6864d32b7a32cf52f100b5a94de171d7a5
Paulo Degering Rosa Junior <paulodrjr@hotmail.com>
2024-06-04 11:06:32 -0300

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wifi_scan/wifi_scan.dart';
+import 'package:wifi_iot/wifi_iot.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -19,84 +23,98 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
-  StreamSubscription<List<WiFiAccessPoint>>? subscription;
+  String? currentSSID;
+  bool isScanning = false;
   bool shouldCheckCan = true;
-
-  bool get isStreaming => subscription != null;
-
-  Future<void> _startScan(BuildContext context) async {
-    // check if "can" startScan
-    if (shouldCheckCan) {
-      // check if can-startScan
-      final can = await WiFiScan.instance.canStartScan();
-      // if can-not, then show error
-      if (can != CanStartScan.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
-        return;
-      }
-    }
-
-    // call startScan API
-    final result = await WiFiScan.instance.startScan();
-    if (mounted) kShowSnackBar(context, "startScan: $result");
-    // reset access points.
-    setState(() => accessPoints = <WiFiAccessPoint>[]);
-  }
-
-  Future<bool> _canGetScannedResults(BuildContext context) async {
-    if (shouldCheckCan) {
-      // check if can-getScannedResults
-      final can = await WiFiScan.instance.canGetScannedResults();
-      // if can-not, then show error
-      if (can != CanGetScannedResults.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
-        accessPoints = <WiFiAccessPoint>[];
-        return false;
-      }
-    }
-    return true;
-  }
-
-  Future<void> _getScannedResults(BuildContext context) async {
-    if (await _canGetScannedResults(context)) {
-      // get scanned results
-      final results = await WiFiScan.instance.getScannedResults();
-      setState(() => accessPoints = results);
-    }
-  }
-
-  Future<void> _startListeningToScanResults(BuildContext context) async {
-    if (await _canGetScannedResults(context)) {
-      subscription = WiFiScan.instance.onScannedResultsAvailable
-          .listen((result) => setState(() => accessPoints = result));
-    }
-  }
-
-  void _stopListeningToScanResults() {
-    subscription?.cancel();
-    setState(() => subscription = null);
-  }
+  String? routerIpAddress;
+  final String ssidPrefix = '';
 
   @override
-  void dispose() {
-    super.dispose();
-    // stop subscription for scanned results
-    _stopListeningToScanResults();
+  void initState() {
+    super.initState();
+    _getScannedResults();
   }
 
-  // build toggle with label
-  Widget _buildToggle({
-    String? label,
-    bool value = false,
-    ValueChanged<bool>? onChanged,
-    Color? activeColor,
-  }) =>
-      Row(
-        children: [
-          if (label != null) Text(label),
-          Switch(value: value, onChanged: onChanged, activeColor: activeColor),
-        ],
-      );
+  Future<void> _getScannedResults() async {
+    // Obter resultados do scan
+    final results = await WiFiScan.instance.getScannedResults();
+    setState(() {
+      accessPoints = results;
+    });
+  }
+
+
+  Future<void> _scanAndFetchResults(BuildContext context) async {
+    if (isScanning) {
+      kShowSnackBar(context, "Scan already in progress");
+      return;
+    }
+
+    if (!await Permission.location.request().isGranted) {
+      _showSnackBar(context, "Permissão de localização negada");
+      return;
+    } 
+
+    setState(() {
+      isScanning = true;
+    });
+
+    try {      
+      WiFiForIoTPlugin.getSSID().then((value) => currentSSID = value!);
+      // Check if "can" startScan
+      if (shouldCheckCan) {
+        final can = await WiFiScan.instance.canStartScan();
+        if (can != CanStartScan.yes) {
+          if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
+          setState(() {
+            isScanning = false;
+          });
+          return;
+        }
+      }
+
+      // Call startScan API
+      final startResult = await WiFiScan.instance.startScan();
+      if (startResult != true) {
+        if (mounted) kShowSnackBar(context, "Failed to start scan");
+        setState(() {
+          isScanning = false;
+        });
+        return;
+      }
+      if (mounted) kShowSnackBar(context, "startScan: $startResult");
+
+      // Wait a moment to allow scan to complete
+      await Future.delayed(Duration(seconds: 2));
+
+      // Check if "can" get scanned results
+      if (shouldCheckCan) {
+        final can = await WiFiScan.instance.canGetScannedResults();
+        if (can != CanGetScannedResults.yes) {
+          if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
+          setState(() {
+            accessPoints = <WiFiAccessPoint>[];
+            isScanning = false;
+          });
+          return;
+        }
+      }
+
+      // Get scanned results
+      final results = await WiFiScan.instance.getScannedResults();
+      setState(() {
+        accessPoints = ssidPrefix != '' ? results
+          .where((ap) => ap.ssid.startsWith(ssidPrefix)) // Filtrar por SSID
+          .toList() : results;
+      });
+    } catch (e) {
+      kShowSnackBar(context, "Error occurred: $e");
+    } finally {
+      setState(() {
+        isScanning = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -104,13 +122,6 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Basico Aroma - Scanner WiFi'),
-          actions: [
-            _buildToggle(
-                label: "Check can?",
-                value: shouldCheckCan,
-                onChanged: (v) => setState(() => shouldCheckCan = v),
-                activeColor: Colors.purple)
-          ],
         ),
         body: Builder(
           builder: (context) => Padding(
@@ -119,40 +130,29 @@ class _MyAppState extends State<MyApp> {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.perm_scan_wifi),
-                      label: const Text('SCAN'),
-                      onPressed: () async => _startScan(context),
-                    ),
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.refresh),
-                      label: const Text('GET'),
-                      onPressed: () async => _getScannedResults(context),
-                    ),
-                    _buildToggle(
-                      label: "STREAM",
-                      value: isStreaming,
-                      onChanged: (shouldStream) async => shouldStream
-                          ? await _startListeningToScanResults(context)
-                          : _stopListeningToScanResults(),
-                    ),
-                  ],
+                Center(
+                  child: ElevatedButton.icon(
+                    icon: const Icon(Icons.perm_scan_wifi),
+                    label: isScanning ? const Text('Scaneando...') : const Text('Escanear Wi-Fi'),
+                    onPressed: isScanning ? null : () async => _scanAndFetchResults(context),
+                  ),
                 ),
                 const Divider(),
                 Flexible(
                   child: Center(
                     child: accessPoints.isEmpty
                         ? const Text("NO SCANNED RESULTS")
-                        : ListView.builder(
+                        : ListView.separated(
                             itemCount: accessPoints.length,
-                            itemBuilder: (context, i) =>
-                                _AccessPointTile(
-                                  accessPoint: accessPoints[i],
-                                  onTap: () => _handleTap(context, accessPoints[i]), // Add onTap handler
-                                ),
+                            itemBuilder: (context, i) => _AccessPointTile(
+                              accessPoint: accessPoints[i],
+                              accessPoints: accessPoints,
+                              onTap: () => _handleTap(context, accessPoints[i]),
+                              parentContext: context,
+                            ),
+                            separatorBuilder: (context, index) => const Divider(
+                              color: Colors.grey,
+                            ),
                           ),
                   ),
                 ),
@@ -212,25 +212,11 @@ class _MyAppState extends State<MyApp> {
 class _AccessPointTile extends StatelessWidget {
   final WiFiAccessPoint accessPoint;
   final VoidCallback onTap;
+  final BuildContext parentContext;
+  final List<WiFiAccessPoint> accessPoints;
 
-  const _AccessPointTile({Key? key, required this.accessPoint, required this.onTap})
+  const _AccessPointTile({Key? key, required this.accessPoint, required this.accessPoints, required this.onTap, required this.parentContext})
       : super(key: key);
-
-  // build row that can display info, based on label: value pair.
-  Widget _buildInfo(String label, dynamic value) => Container(
-        decoration: const BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.grey)),
-        ),
-        child: Row(
-          children: [
-            Text(
-              "$label: ",
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Expanded(child: Text(value.toString()))
-          ],
-        ),
-      );
 
   @override
   Widget build(BuildContext context) {
@@ -241,12 +227,182 @@ class _AccessPointTile extends StatelessWidget {
     return ListTile(
       visualDensity: VisualDensity.compact,
       leading: Icon(signalIcon),
-      title: Text(title),
-      subtitle: Text(accessPoint.capabilities),
-      onTap: onTap, // Call the provided onTap function
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title),
+                Text(accessPoint.capabilities, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              ],
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              _canConnectWithCurrentSSID().then((value) {
+                if (value) _showPasswordDialog(parentContext, accessPoint);
+              });              
+              //_showPasswordDialog(parentContext, accessPoint);
+              
+            },
+            child: const Text('Configurar'),
+          ),
+        ],
+      ),
+      onTap: onTap,
     );
   }
+
+  void _showPasswordDialog(BuildContext context, WiFiAccessPoint accessPoint) {
+    final TextEditingController passwordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Informe a senha da rede ATUAL'),
+        content: TextField(
+          controller: passwordController,
+          decoration: const InputDecoration(
+            labelText: 'Senha',
+            border: OutlineInputBorder(),
+          ),
+          obscureText: true,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _connectToNetwork(parentContext, accessPoint.ssid, passwordController.text);
+            },
+            child: const Text('Ok'),            
+          ),          
+        ],
+      ),
+    );
+  }
+
+  Future<bool> _canConnectWithCurrentSSID() async {
+    final info = NetworkInfo();
+    String? currSSID = await info.getWifiName();
+
+    if (currSSID == null) {
+      _showSnackBar(parentContext, "Não foi possível obter o SSID da rede atual");
+      return false;
+    }
+
+    //replace "" with "
+    currSSID = currSSID.replaceAll("\"", "");
+    
+    WiFiAccessPoint? currentAccessPoint;
+    //for each access point in accessPoints, if the ssid matches the ssid of the access point we want to connect to, set currentAccessPoint to that access point
+    for (int i = 0; i < accessPoints.length; i++) {
+      if (accessPoints[i].ssid == currSSID) {
+        currentAccessPoint = accessPoints[i];
+      }
+    }
+    
+    if (currentAccessPoint != null && currentAccessPoint.frequency > 3000){
+      _showSnackBar(parentContext, "O aparelho está conectado a uma rede de 5GHz. Por favor, conecte a uma rede de 2.4GHz para continuar.");
+      return false;
+    }
+
+    return true;
+  }
+
+
+  Future<void> _connectToNetwork(BuildContext context, String ssid, String password) async {    
+    
+    final info = NetworkInfo();
+    String? currSSID = await info.getWifiName();
+
+    String json = '{"ssid":"$currSSID","password":"$password"}';
+
+    // Solicitar permissão de localização em tempo de execução
+    if (await Permission.location.request().isGranted) {
+
+      // Desconectar do Wi-Fi atual antes de tentar conectar-se a um novo Wi-Fi
+      try {
+        await WiFiForIoTPlugin.disconnect();
+      } catch (e) {
+        _showSnackBar(parentContext, 'Erro ao desconectar do Wi-Fi atual: $e');
+        return;
+      }
+
+      // Connect to Wi-Fi network
+      try {
+        String deviceIP = await connect(ssid, "ba%23Aa4");
+        // result = "Router IP Address 1: $result";
+        // print(result);
+        
+        // Get the device's IP address after successful connection
+        // final deviceIpAddress = await WiFiForIoTPlugin.getIP();
+        // print(_getRouterIpAddress(deviceIpAddress!));
+
+        // result += "\nRouter IP Address 2: ${_getRouterIpAddress(deviceIpAddress)}";
+        _showSnackBar(parentContext, "Conectado com sucesso à rede Wi-Fi: $deviceIP");
+
+        final http.Response response = await http.post(
+          Uri.parse('http://BasicoAroma/Configuracao'),
+          body: json,
+        );
+
+        if (response.statusCode == 200) {
+          _showSnackBar(parentContext, 'Configuração enviada com sucesso!');
+          await WiFiForIoTPlugin.disconnect();
+          //remove the network from the list of available networks
+          accessPoints.removeWhere((element) => element.ssid == ssid);
+        } else {
+          _showSnackBar(parentContext, 'Falha ao enviar a configuração. Código de status: ${response.statusCode}');
+        }
+
+      } catch (e) {
+        _showSnackBar(parentContext, 'Erro ao conectar à rede Wi-Fi: $e');
+        return;
+      }
+      
+    }else{
+      _showSnackBar(parentContext, "Permissão de localização negada");
+    }
+  }
+
+  Future<String> connect(String ssid, String pwd) async {
+    try {
+      bool result = await WiFiForIoTPlugin.connect(ssid, withInternet: false, security: NetworkSecurity.WPA, password: pwd, );
+      print("connecting");
+      if (result) {
+        WiFiForIoTPlugin.forceWifiUsage(true);
+        return "Successfully connected to $ssid (IP: ${await WiFiForIoTPlugin.getIP()})";
+      } else {
+        return "There was an error connecting to that wifi!";
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 }
+
+void _showSnackBar(BuildContext context, String message) {
+    if (context != null && ScaffoldMessenger.of(context).mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message)),
+      );
+    }
+  }
+
+// Function to extract router's IP address from device's IP address
+String _getRouterIpAddress(String deviceIpAddress) {
+  final List<String> parts = deviceIpAddress.split('.');
+  // Replace the last part of device's IP address with '1' to get router's IP address
+  parts[3] = '1';
+  return parts.join('.');
+}
+
 
 /// Show snackbar.
 void kShowSnackBar(BuildContext context, String message) {
